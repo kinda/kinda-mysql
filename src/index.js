5cdff24606b7275accbb7014fa4bbf5e8451424b
Manuel Vila <mvila@3base.com>
2015-05-29 13:10:39 +0800

    switch to ES6

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 'use strict';
 
-var mysql = require('mysql');
+let mysql = require('mysql');
+let KindaObject = require('kinda-object');
 
-var kindaMySQL = {
-  createPool: function(options) {
-    var mysqlPool = mysql.createPool(options);
-    var pool = {
-      getConnection: function() {
+let KindaMySQL = KindaObject.extend('KindaMySQL', function() {
+  this.createPool = function(options) {
+    let mysqlPool = mysql.createPool(options);
+
+    let pool = {
+      getConnection() {
         return function(cb) {
           mysqlPool.getConnection(function(err, mysqlConnection) {
             if (err) return cb(err);
-            var connection = {
-              query: function(sql, values) {
-                return function(cb) {
-                  mysqlConnection.query(sql, values, function(err, res) {
-                    cb(err, res);
+            let connection = {
+              query(sql, values) {
+                return function(queryCb) {
+                  mysqlConnection.query(sql, values, function(queryErr, res) {
+                    queryCb(queryErr, res);
                   });
                 };
               },
@@ -27,34 +29,37 @@ var kindaMySQL = {
           });
         };
       },
-      query: function(sql, values) {
+
+      query(sql, values) {
         return function(cb) {
           mysqlPool.query(sql, values, function(err, res) {
             cb(err, res);
           });
         };
       },
+
       on: mysqlPool.on.bind(mysqlPool),
+
       format: mysql.format.bind(mysql),
+
       escape: mysql.escape.bind(mysql),
+
       escapeId: mysql.escapeId.bind(mysql),
-      end: function() {
+
+      end() {
         return function(cb) {
           mysqlPool.end(cb);
         };
       }
     };
     return pool;
-  },
-  format: mysql.format.bind(mysql),
-  escape: mysql.escape.bind(mysql),
-  escapeId: mysql.escapeId.bind(mysql)
-};
+  };
 
-var KindaMySQL = {
-  create: function() {
-    return kindaMySQL;
-  }
-};
+  this.format = mysql.format.bind(mysql);
+
+  this.escape = mysql.escape.bind(mysql);
+
+  this.escapeId = mysql.escapeId.bind(mysql);
+});
 
 module.exports = KindaMySQL;
